532fa3e3375102ee52b74350e977d112bb6548f5
Cody Taylor <codyalantaylor@gmail.com>
2018-08-08 11:27:27 -0500

    added URL logging for optimization purposes

--- src/background/background.js
+++ src/background/background.js
@@ -15,4 +15,24 @@ chrome.runtime.onMessage.addListener(function(request, sender) {
       });
     }
   }
+  // This conditional statement is only for debugging purposes. It
+  // is meant to log all URLs on which Cookie removes a cookie notice
+  // in order to be checked for proper operation after a decent number
+  // of URLs have been logged. Also the text of the notices should be
+  // scraped and their keywords analyzed for optimizing Cookie.
+  if (request.type == 'storage'){
+    // chrome.storage.local.set({'cookie': ''}); // use to clear localStorage
+    chrome.storage.local.get(['cookie'], function(result){
+      if(result.cookie.includes(request.options.url)){
+        // do nothing
+        console.log(result.cookie);
+      }
+      else{
+        chrome.storage.local.set({'cookie': result.cookie + ', '+ (request.options.url)}, function(result) {
+          console.log(result.cookie);
+        });
+      }
+    });
+  }
+  // End debugging block
 });
